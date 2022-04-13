cee641d19901599392c661a0d6e1d28c326f273a
Agata Mieniuk <101498669+agatamieniuk@users.noreply.github.com>
2022-04-13 09:10:33 +0200

    Update Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -1,7 +1,9 @@
 public class Rocket implements SpaceShip{
     int rocketCurrentWeight;
     int maxWeight;
+    int weight;
     double cost;
+
 
     @Override
     public boolean launch() {
@@ -14,8 +16,8 @@ public class Rocket implements SpaceShip{
     }
 
     @Override
-    public boolean canCarry(Item item) { //can carry item or it will exceed the weight limit.
-        if(maxWeight<=item.weight + rocketCurrentWeight){
+    public final boolean canCarry(Item item) { //can carry item or it will exceed the weight limit.
+        if(maxWeight>=item.weight + rocketCurrentWeight){
             return true;
         }else{
             return false;
@@ -23,8 +25,18 @@ public class Rocket implements SpaceShip{
     }
 
     @Override
-    public int carry(Item item) { // updates the current weight
+    public final int carry(Item item) { //updates the current weight
         rocketCurrentWeight = item.weight + rocketCurrentWeight;
         return rocketCurrentWeight;
     }
+
+    @Override
+    public String toString() {
+        return "Rocket{" +
+                "rocketCurrentWeight=" + rocketCurrentWeight +
+                ", maxWeight=" + maxWeight +
+                ", cost=" + cost +
+                '}';
+    }
 }
+//final - nie pozwala na nadpisanie pozniej
