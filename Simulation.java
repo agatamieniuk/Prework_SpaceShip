7d92ba1654e6ef72cb29a93d3a719d75c48c06a7
Agata Mieniuk <101498669+agatamieniuk@users.noreply.github.com>
2022-04-13 09:10:53 +0200

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -1,66 +1,74 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Simulation {
 
-    ArrayList<Item> loadItems(String fileName) throws Exception{
-        ArrayList itemList = new ArrayList();
+    List<Item> loadItems(String fileName) throws Exception {
+        List<Item> itemList = new ArrayList();
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
-        while(scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             Item item = new Item();
             String line = scanner.nextLine();
             String[] lineArray = line.split("="); //split txt by"=" sigh
             item.name = lineArray[0];
             item.weight = Integer.parseInt(lineArray[1]);
             itemList.add(item);
-        }return itemList;
+        }
+        return itemList;
     }
 
     //!!!!!
-    ArrayList<Rocket> loadU1(ArrayList<Item> itemList){
-        ArrayList<Rocket> u1List = new ArrayList();
-        Rocket rocketU1 = new Rocket();
-            for(Item item: itemList) {
-                if(rocketU1.canCarry(item)){
-                    rocketU1.carry(item);
-                } else { //<=!!!
-                    u1List.add(rocketU1);
-                    rocketU1 = new Rocket();
-                    rocketU1.carry(item);
-                }
-        }
-            if (itemList.size() != 0){
-                u1List.add(rocketU1);
-            } return u1List; //<=!!!
-    }
-    //!!!!!
-    ArrayList<Rocket> loadU2(ArrayList<Item> itemList){
-        ArrayList<Rocket> u2List = new ArrayList();
-        Rocket rocketU2 = new Rocket();
-        for(Item item: itemList) {
-            if(rocketU2.canCarry(item)){
-                rocketU2.carry(item);
+    List<Rocket> loadU1(List<Item> itemList) {
+        List<Rocket> u1List = new ArrayList();
+        Rocket first = new U1();
+        for (Item item : itemList) {
+            if (first.canCarry(item)) {
+                first.carry(item);
             } else { //<=!!!
-                u2List.add(rocketU2);
-                rocketU2 = new Rocket();
-                rocketU2.carry(item);
+                u1List.add(first);
+                first = new U1();
+                first.carry(item);
             }
         }
-        if (itemList.size() != 0){
-            u2List.add(rocketU2);
-        } return u2List; //<=!!!
+        if (itemList.size() != 0) {
+            u1List.add(first);
+        }
+        return u1List; //<=!!!
     }
 
-    double runSimulation(ArrayList<Rocket> rocketList){
+    //!!!!!
+    List<Rocket> loadU2(List<Item> itemList) {
+        List<Rocket> u2List = new ArrayList();
+        Rocket second = new U2();
+        for (Item item : itemList) {
+            if (second.canCarry(item)) {
+                second.carry(item);
+            } else { //<=!!!
+                u2List.add(second);
+                second = new U2();
+                second.carry(item);
+            }
+        }
+        if (itemList.size() != 0) {
+            u2List.add(second);
+        }
+        return u2List; //<=!!!
+    }
+
+    double runSimulation(List<Rocket> rocketList) {
+        System.out.println("rocketList.size() = " + rocketList.size());
         double budget = 0;
-        for (Rocket rocket: rocketList){
+        for (Rocket rocket : rocketList) {
+            System.out.println("rocket = " + rocket);
             budget = budget + rocket.cost;
-            while(!rocket.land() || !rocket.launch()){
+            while (!rocket.launch()||!rocket.land() ) {
                 budget += rocket.cost;
             }
-        }return budget;
+        }
+        return budget;
     }
 
 }
